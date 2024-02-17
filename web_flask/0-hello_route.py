62ff1e612a30141ebccc8769559e43df3edd10de
annerachael <annerwenji@gmail.com>
2024-02-16 23:11:27 -0800

    flask

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
-
 """
-Script to display “Hello HBNB!”
+Start the flask web framework
+The application listens on 0.0.0.0, port 5000
+Routes:
+    /:Display 'Hello HBNB'
 """
-
-
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/', strict_slashes=False)
-def hello_hbnb():
-    """ 
-    Display HBNB 
-    """
-    return "Hello HBNB!"
 
-if __name__=='__main__':
-    app.run(host='0.0.0.0')
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """Display 'Hello HBNB'"""
+    return 'Hello HBNB!'
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
