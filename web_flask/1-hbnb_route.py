82584ca2d849a1625c05b84f7299f1c41462df56
annerachael <annerwenji@gmail.com>
2024-02-18 08:39:14 -0800

    flask

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,24 +1,27 @@
 #!/usr/bin/python3
 """
-script that runs flask
+Script that runs a flask app
 """
 from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     """
-    function that returns url
+    Function that returns on url
     """
     return "Hello HBNB!"
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb_school():
     """
-    function that returns url
+    Function that returns on url /hbnb
     """
     return "HBNB"
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(host="0.0.0.0")
